4e9fc3da33374ee038cd83dd8bb7b856f2ba0d78
David Olsson <m3rli.olsson@gmail.com>
2017-03-21 15:27:52 +0100

    Updating AssignmentController::show

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -31,16 +31,16 @@ class AssignmentController extends Controller
 
             /*Try our model function*/
             try {
-                $this->AM->getAllAO();
+                $all_ass_own = Assignment::all()->owners;
             } catch (\Exception $e) {
                 return 'Something wrong with the connection.';
             }
 
             /*Get all assignments and owners with the values that we want.
             After that decode it to json then return it.*/
-            $all_ass_own = $this->AM->getAllAO();
+            // $all_ass_own = $this->AM->getAllAO();
 
-            $all_ass_own_json = $all_ass_own->toJson();
+            // $all_ass_own_json = $all_ass_own->toJson();
 
             return $all_ass_own_json;
         }else{
@@ -49,32 +49,7 @@ class AssignmentController extends Controller
 
     
     }
-
-    public function create()
-    {
-        /*Get all the owners for the assignment creation*/
-        try {
-            $all_owners = $this->OM->getAllOwners();
-        } catch (\Exception $e) {
-            return 'Something wrong with the connection.';
-        }
-
-        $all_owns_json = $all_owners->toJson();
-
-        /*We don't have a form with a post method. 
-          So we have almost the store function here aswell.*/
-
-        // $o_count = count($all_owners);
-        // $request = (object) array('owner_id' => rand(0,$o_count), 'device_type'=>'iphone','log'=>'något','resolved'=>0);
-        // try {
-        //     $this->AM->postAssignment($request);
-        // } catch (\Exception $e) {
-        //     return 'Something wrong with the connection.';
-        // }
-
-
-        return $all_owners;
-    }   
+   
 
 
     /*We are going to post the data into database 
@@ -82,19 +57,17 @@ class AssignmentController extends Controller
     public function store(Request $request)
     {
 
-        $request = (object) array('owner_id' => '1', 'device_type'=>'iphone','log'=>'något','resolved'=>0);
-
         /*See if we even have $request*/
         if (!isset($request)) {
-            return "Something went wrong.";
+            return "There is no object.";
         }if (!isset($request->owner_id)) {
-            return "Something went wrong.";
+            return "Did not found a owner.";
         }if (!isset($request->device_type)) {
-            return "Something went wrong.";
+            return "Did not found any device.";
         }if (!isset($request->log)) {
-            return "Something went wrong.";
+            return "Did not found any log.";
         }if (!isset($request->resolved)) {
-            return "Something went wrong.";
+            return "Did not if the device are resolved.";
         }else{
 
             /*Test if we can post it to the model*/
@@ -118,51 +91,22 @@ class AssignmentController extends Controller
     public function show($id)
     {
         try {
-            $assignment = $this->AM->getSpecAssOwn($id);
+            $assignment = Assignment::find($id);
+            $owner = Assignment::find($id)->owner;
         } catch (\Exception $e) {
             return 'Something wrong with the connection.';
         }
         if (!isset($assignment)) {
-            return 'Something wrong with the connection.';
+            return 'There is no assignment object to get from database.';
+        }if (!isset($owner)) {
+            return 'There is no owner object to get from database.';
         }else{
+
             $assignment_json = $assignment->toJson();
+            $owner_json = $owner->toJson();
 
-            return $assignment_json;
-        }
-
-    }
-
-    public function edit($id)
-    {
-
-        if (DB::connection()->getPdo()) {
-
-            
-            /*getSpecAssOwn will get specific assignment and join owner with it.*/
-            try {
-                $assignment_owner = $this->AM->getSpecAssOwn($id);
-            } catch (\Exception $e) {
-                return 'Something wrong with the connection.';
-            }
-
-            if (!isset($assignment_owner)) {
-                return 'Something wrong with the connection.';
-            }else{
-                $assignment_owner_json = $assignment_owner->toJson();
-
-                /*No put method, uncomment to test*/
-                /*This is our request from our "view".
-                  This update will change the owner_id*/
-
-                // $request = array('owner_id' => 4);
-                // $this->AM->updateAssignment($request,$id);
-                // return redirect('assignments/'.$id);
-                return $assignment_owner_json;
-            }
-        }else{
-            return 'Something wrong with the connection.';
-        }
-
+            return $assignment_json . $owner_json;
+        } 
 
     }
 
@@ -170,9 +114,6 @@ class AssignmentController extends Controller
       after the PUT method on URI assignments/id.*/
     public function update(Request $request, $id)
     {
-        /*This is our request from our "view"*/
-        $request = array('owner_id' => 4);
-
 
         if (!isset($request)) {
             return 'Something went wrong.';
